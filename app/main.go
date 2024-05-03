8acdb4bb4eec642759deae25a6081e7f6c41e268
nikita <nicita.zinkevitch@yandex.ru>
2024-05-03 13:24:29 +0300

    Вычисление условной энтропии двух фиксированных вероятностных ансамблей

--- app/main.go
+++ app/main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main() {
-	data, err := ioutil.ReadFile("example/bib")
+	data, err := ioutil.ReadFile("example/book1")
 	if err != nil {
 		fmt.Println("Error reading file: ", err.Error())
 		os.Exit(1)
 	}
 	freqs, probs := entropy.GetFreqsProbs(data)
 	entr := entropy.GetEntropy(probs)
-	cond := entropy.GetConditionalProbs(data, freqs)
-	condEntr := entropy.GetCondEntropy(probs, cond)
-	fmt.Println(cond)
-	fmt.Println("Entropy: ", entr)
-	fmt.Println("Conditional entropy: ", condEntr)
+	condProbs, condFreqs := entropy.GetCondProbs(data, freqs)
+	condEntr := entropy.GetCondEntropy(probs, condProbs)
+	condProbsXX := entropy.GetCondProbsXX(data, condFreqs)
+	condEntrXX := entropy.GetCondEntropyXX(probs, condProbsXX)
+	fmt.Println(condProbsXX)
+	fmt.Println("Entropy H(X): ", entr)
+	fmt.Println("Conditional entropy H(X|X): ", condEntr)
+	fmt.Println("Conditional entropy H(X|XX): ", condEntrXX)
 }
--- entropy.go
+++ entropy.go
@@ -26,7 +26,7 @@ func GetEntropy(probs [256]float64) float64 {
 	return entropy
 }
 
-func GetConditionalProbs(seq []byte, freqs [256]float64) map[byte]map[byte]float64 {
+func GetCondProbs(seq []byte, freqs [256]float64) (map[byte]map[byte]float64, map[byte]map[byte]float64) {
 	var condFreqs, condProbs = make(map[byte]map[byte]float64), make(map[byte]map[byte]float64)
 	for i := 1; i < len(seq); i++ {
 		cur := seq[i]
@@ -46,7 +46,7 @@ func GetConditionalProbs(seq []byte, freqs [256]float64) map[byte]map[byte]float
 		}
 	}
 
-	return condProbs
+	return condProbs, condFreqs
 }
 
 func GetCondEntropy(probs [256]float64, condProbs map[byte]map[byte]float64) float64 {
@@ -57,6 +57,51 @@ func GetCondEntropy(probs [256]float64, condProbs map[byte]map[byte]float64) flo
 			temp += condProb * math.Log2(condProb)
 		}
 		entropy += prob * temp
+	}
+	return -entropy
+}
+
+func GetCondProbsXX(seq []byte, condFreqs map[byte]map[byte]float64) map[byte]map[byte]map[byte]float64 {
+	var condFreqsXX, condProbsXX = make(map[byte]map[byte]map[byte]float64), make(map[byte]map[byte]map[byte]float64)
+	for i := 2; i < len(seq); i++ {
+		cur := seq[i]
+		prev := seq[i-1]
+		prev2 := seq[i-2]
+		if condFreqsXX[prev2] == nil {
+			condFreqsXX[prev2] = make(map[byte]map[byte]float64)
+		}
+		if condFreqsXX[prev2][prev] == nil {
+			condFreqsXX[prev2][prev] = make(map[byte]float64)
+		}
+		condFreqsXX[prev2][prev][cur]++
+	}
+
+	for prev2 := range condFreqsXX {
+		if condProbsXX[prev2] == nil {
+			condProbsXX[prev2] = make(map[byte]map[byte]float64)
+		}
+		for prev := range condFreqsXX[prev2] {
+			if condProbsXX[prev2][prev] == nil {
+				condProbsXX[prev2][prev] = make(map[byte]float64)
+			}
+			for cur := range condFreqsXX[prev2][prev] {
+				condProbsXX[prev2][prev][cur] = condFreqsXX[prev2][prev][cur] / condFreqs[prev2][prev]
+			}
+		}
+	}
+	return condProbsXX
+}
+
+func GetCondEntropyXX(probs [256]float64, condProbsXX map[byte]map[byte]map[byte]float64) float64 {
+	var entropy float64
+	for prev2 := range condProbsXX {
+		for prev := range condProbsXX[prev2] {
+			var temp float64
+			for _, condProb := range condProbsXX[prev2][prev] {
+				temp += condProb * math.Log2(condProb)
+			}
+			entropy += probs[prev2] * probs[prev] * temp
+		}
 	}
 	return -entropy
 }
